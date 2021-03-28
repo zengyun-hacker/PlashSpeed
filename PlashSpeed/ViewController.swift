673d5a6296212a510a3bfa26c627ed9bde0321d4
Programmer.Du <zengyun.hacker@gmail.com>
2021-03-28 23:21:08 +0800

    project initial

---
+++ PlashSpeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlashSpeed
+//
+//  Created by Du on 2021/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
